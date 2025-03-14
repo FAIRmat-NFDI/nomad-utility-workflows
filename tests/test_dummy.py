7e4fa52a33bc3997cc60e2391b645ef6cd2c8bc4
ndaelman <ndaelman@physik.hu-berlin.de>
2025-03-14 14:27:01 +0100

    - Build out test infrastructure - Add test for path

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,2 +1,15 @@
-def test_dummy():
-    assert True
+import nomad_utility_workflows.utils.workflows as nuw
+
+
+def test_main_path():
+    ns = nuw.NomadSection(
+        path_info={
+            'mainfile_path': 'vibrational_analysis.archive.yaml',
+            'entry_id': 'Qq0VGiABIgx9HQtpYL4RLdONuUp1',
+            'archive_path': 'data'
+        },        
+    )
+    assert ns.full_path == '/entries/Qq0VGiABIgx9HQtpYL4RLdONuUp1/archive#/data'
+
+    ns.path_info['upload_id'] = 'J3lTsIQBRxuGBJQFd6uaKA'
+    assert ns.full_path == '/uploads/J3lTsIQBRxuGBJQFd6uaKA/archive/Qq0VGiABIgx9HQtpYL4RLdONuUp1#/data'
